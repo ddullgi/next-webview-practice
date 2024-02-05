eca934b46f6927a8f66c352f445afb73523a152a
황상윤 <s_infinite@naver.com>
2024-02-05 20:21:36 +0900

    feat. RN to web postmessage test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  useEffect(() => {
+    document.addEventListener("message", (e: any) => alert(e.data));
+  }, []);
+
   const sendToRN = () => {
     if (window.ReactNativeWebView) {
       // RN에서 데이터는 반드시 문자열로 받을 수 있기 때문에
